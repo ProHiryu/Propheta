95f34a3f4d59b9c3a1926dec73082fe71c6a93de
ProHiryu <songheqi1996@gmail.com>
2016-12-01 14:19:54 +0800

    pandas

--- Data_Prepocessing/prepocessing.py
+++ Data_Prepocessing/prepocessing.py
@@ -1,6 +1,10 @@
 # -*- coding:utf-8 -*-
 import sqlite3
+import pandas as pd
 
 coon = sqlite3.connect('lol.sqlite')
-cur = coon.cursor()
+sql = "select * from Games"
+
+df = pd.read_sql(sql,coon,index_col='id')
+print(df.head(),len(df))
 
